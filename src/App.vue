253eb9906fe642dd6ae8aacfb384b256faed22b1
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-21 17:15:54 -0500

    Part 1 complete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import DatePicker from './components/DatePicker.vue'
+
+const dates = ref({
+  startDate: null,
+  endDate: null
+})
+const setDates = (selectedDates) => {
+  dates.value.startDate = new Date(selectedDates[0]).toDateString()
+  dates.value.endDate = new Date(selectedDates[1]).toDateString()
+}
+</script>
 
 <template>
-  <div>App</div>
+  <h1>Event Dashboard</h1>
+  <h3 v-if="dates">{{ dates.startDate }} - {{ dates.endDate }}</h3>
+  <DatePicker @selectedDates="setDates" />
 </template>
 
 <style scoped></style>
---
+++ src/components/DatePicker.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Calendar from 'primevue/calendar'
+import { ref } from 'vue'
+
+const emits = defineEmits(['selectedDates'])
+
+const dates = ref()
+const selectedDates = (date) => {
+  emits('selectedDates', date)
+}
+</script>
+
+<template>
+  <div>
+    <Calendar v-model="dates" selection-mode="range" inline @date-select="selectedDates(dates)" />
+  </div>
+</template>
+
+<style scoped></style>
