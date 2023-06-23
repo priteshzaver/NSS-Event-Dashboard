57b69e62ea111d13b292f3c41727ada3f7761c77
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-23 12:01:23 -0500

    part 4 complete. All three cards combined into 1 component

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import 'primeflex/primeflex.css'
 import 'primeicons/primeicons.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faFootball, faTheaterMasks } from '@fortawesome/free-solid-svg-icons'
+import { faFootball, faTheaterMasks, faMusic } from '@fortawesome/free-solid-svg-icons'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-library.add(faFootball, faTheaterMasks)
+library.add(faFootball, faTheaterMasks, faMusic)
 
 const app = createApp(App)
 
