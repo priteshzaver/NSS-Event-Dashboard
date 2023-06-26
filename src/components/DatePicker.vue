a5546fe3c32587bf0981618bded01b5095ec33bc
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-26 12:22:50 -0500

    DatePicker styling complete

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -11,7 +11,20 @@ const selectedDates = (date) => {
 </script>
 
 <template>
-  <Calendar v-model="dates" selection-mode="range" inline @date-select="selectedDates(dates)" />
+  <Calendar
+    v-model="dates"
+    selection-mode="range"
+    inline
+    @date-select="selectedDates(dates)"
+    :pt="{
+      root: { class: 'w-full' },
+      header: { class: 'bg-primary' },
+      previousButton: { class: 'bg-primary' },
+      nextButton: { class: 'bg-primary' },
+      monthTitle: { class: 'bg-primary' },
+      yearTitle: { class: 'bg-primary' }
+    }"
+  />
 </template>
 
 <style scoped></style>
